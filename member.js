222c77dd9e218923a34822e6d09f5a37bee2434c
Udhayc <45636937+Udhayc@users.noreply.github.com>
2025-02-06 05:31:28 +0000

    copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember()
+ {
+  console.log("skillsMember");
+  var skills = document.getElementById("skills");
+  skills.classList.remove("d-none");
+  skills.classList.add("d-block");
+}
